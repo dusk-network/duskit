4168aa6dafc44d0de1a643de8b1e717607f06681
Alex Panturu <panturu.alexandru@gmail.com>
2024-03-26 13:23:56 +0200

    move IntersectionObserver mock

--- packages/components/vite-setup.js
+++ packages/components/vite-setup.js
@@ -2,9 +2,11 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 import { expect, vi } from "vitest";
 import "jsdom-worker";
 // prettier-ignore
-import { mockIntersectionObserver, mockResizeObserver } from "@duskit/test-helpers";
+import { IntersectionObserver, ResizeObserver } from "./src/mocks";
+
+vi.mock("./src/mocks/IntersectionObserver");
+
+global.IntersectionObserver = IntersectionObserver;
+global.ResizeObserver = ResizeObserver;
 
 expect.extend(matchers);
-
-vi.stubGlobal("IntersectionObserver", mockIntersectionObserver);
-vi.stubGlobal("ResizeObserver", mockResizeObserver);
