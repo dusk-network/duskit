2a79d6fcd787a8ff2d70b0733c92755801670410
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2025-06-23 16:00:47 +0200

    Remove `currency` package
    Resolves #133

--- packages/currency/index.js
+++
@@ -1,6 +0,0 @@
-export { default as createCurrencyFormatter } from "./src/createCurrencyFormatter";
-export { default as createFeeFormatter } from "./src/createFeeFormatter";
-export { default as createTransferFormatter } from "./src/createTransferFormatter";
-export { default as currencies } from "./src/currencies";
-export { default as duskToLux } from "./src/duskToLux";
-export { default as luxToDusk } from "./src/luxToDusk";
