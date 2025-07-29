677c3bfa9f282f003a245d5ab695cba60a00e323
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2025-07-29 14:26:46 +0200

    Update `Button` story to allow changing all props
    - updated `--control-bg-color-hover` in `@duskit/css` to match the right colour
    
    Resolves #166

--- apps/workshop/src/stories/components/button/Button.story.svelte
+++ apps/workshop/src/stories/components/button/Button.story.svelte
@@ -1,33 +1,92 @@
 <script>
-  import { logEvent } from "histoire/client";
   import { Button } from "@duskit/components";
+  import { logEvent } from "histoire/client";
+  import { mdiHome } from "@mdi/js";
 
   export let Hst;
 
-  let properties = { className: undefined, disabled: false, text: "click me" };
+  /** @typedef {import("svelte").ComponentProps<Button>} ButtonProps */
+
+  let currentProps;
+
+  /** @type {ButtonProps["icon"]}*/
+  // eslint-disable-next-line prefer-const -- needs to stay reactive for Svelte bindings
+  let iconProp = {
+    path: mdiHome,
+    position: "before",
+    size: "default",
+  };
+
+  let useIcon = false;
+
+  /** @type {ButtonProps}*/
+  // eslint-disable-next-line prefer-const -- needs to stay reactive for Svelte bindings
+  let properties = {
+    active: false,
+    disabled: false,
+    size: "default",
+    text: "Click me",
+    type: "button",
+    variant: "primary",
+  };
+
+  $: icon = useIcon ? iconProp : undefined;
+  $: currentProps = { ...properties };
 </script>
 
-<svelte:component this={Hst.Story} title="Button" group="components">
+<Hst.Story title="Button" group="components">
   <Button
-    className={properties.className}
+    active={properties.active}
     disabled={properties.disabled}
-    text={properties.text}
+    {icon}
     on:click={(event) => logEvent("click", event)}
+    size={properties.size}
+    text={properties.text}
+    type={properties.type}
+    variant={properties.variant}
   />
 
   <svelte:fragment slot="controls">
-    <svelte:component
-      this={Hst.Checkbox}
-      bind:value={properties.disabled}
-      title="Disabled"
+    <Hst.Text bind:value={properties.text} title="Text" />
+    <Hst.Select
+      options={["button", "toggle"]}
+      title="Type"
+      bind:value={properties.type}
     />
+
+    <Hst.Checkbox bind:value={properties.disabled} title="Disabled" />
+
+    {#if properties.type === "toggle"}
+      <Hst.Checkbox bind:value={properties.active} title="Active" />
+    {/if}
+
+    <Hst.Select
+      options={["default", "small"]}
+      title="Size"
+      bind:value={properties.size}
+    />
+    <Hst.Select
+      options={["primary", "secondary", "tertiary"]}
+      title="Variant"
+      bind:value={properties.variant}
+    />
+
+    <hr />
+
+    <Hst.Checkbox bind:value={useIcon} title="Show icon" />
+
+    {#if useIcon}
+      <Hst.Select
+        disabled={!useIcon}
+        options={["before", "after"]}
+        title="Icon position"
+        bind:value={iconProp.position}
+      />
+    {/if}
+
     <br />
     <hr />
     <br />
-    <svelte:component
-      this={Hst.Json}
-      bind:value={properties}
-      title="Current Props"
-    />
+    <Hst.Json bind:value={currentProps} title="Current Props" />
   </svelte:fragment>
-</svelte:component>
+</Hst.Story>
