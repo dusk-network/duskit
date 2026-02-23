5336d180176d2f31fa32d30e0bf1ffcf049ab27b
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2026-02-23 10:56:14 +0100

    feat(date): Extracted internal time unit logic into a public utility (`getRelativeTimeUnit`)
    Resolves #218

--- packages/date/index.js
+++ packages/date/index.js
@@ -1,2 +1,3 @@
 export { default as getRelativeTimeString } from "./src/getRelativeTimeString";
+export { default as getRelativeTimeUnit } from "./src/getRelativeTimeUnit";
 export { default as unixTsToDate } from "./src/unixTsToDate";
